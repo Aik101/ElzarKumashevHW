3683b81b931444f7bd03394cc249b997419e1ec8
evgeniy <evgeniy@gitlab@sv.skyflabs.ru>
2022-06-07 10:34:20 +0300

    Main entrypoint in programm

---
+++ index.js
@@ -0,0 +1,26 @@
+"use strict";
+
+
+const port = global.process.env.PORT || 3000;
+
+
+const app = require("express")();
+const bodyParser = require("body-parser");
+const db = require("node-json-db");
+
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+
+app.listen(port, () => {
+  console.log(`Start on port ${ port }`);
+  return undefined;
+});
+
+
+app.get("/", (req, res) => {
+  res.render("about");
+  return undefined;
+});
+
